b37f0c3924dd6efc17379952a01d7d7817a42b81
Neura Design Studio <daniel.visualfx@gmail.com>
2015-03-03 20:40:04 -0500

    Added History & donate

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ var config = {
 gulp.task('connect', function() {
     connect.server({
         root: config.outputDir,
-        port:8000,
+        port:8002,
         open: {
             browser: 'Google Chrome'
         },
